ee364b7d764ec7e3c9be6628c7736066ff221de2
aitor <aitor@e451.net>
2016-10-24 15:00:23 +0200

    corrección licencia

--- web/app/themes/colormag/footer.php
+++ web/app/themes/colormag/footer.php
@@ -37,9 +37,9 @@
 											<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
 										</p>
 										<p>
-											Este obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">licencia de Creative Commons Reconocimiento-CompartirIgual 4.0 Internacional</a>. Se permite la utilización de los contenidos manteniendo los términos de la licencia.
+											Los contenidos, salvo mención expresa, se publican bajo <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">licencia de Creative Commons Reconocimiento-CompartirIgual 4.0 Internacional</a>. Se permite la utilización de los contenidos manteniendo los términos de la licencia.
 										</p>
-										</div>
+									</div>
 					</div>
 				</div>
 			</div>
